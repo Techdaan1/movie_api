8c19ac565cf99fd99d0c4c7de1abad9a8fc6ee45
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-09-29 13:40:10 +0200

    added movielist and created Express GET route

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 const express = require('express');
 const app = express();
 
-const http = require('http');
+let topMovies = [
+  {
+    title: 'Schindler’s List',
+    director: 'Steven Spielberg'
+  },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    director: 'Peter Jackson'
+  },
+  {
+    title: 'The Shining',
+    director: 'Stanley Kubrick'
+  },
+  {
+    title: 'Inception',
+    director: 'Christopher Nolan'
+  },
+  {
+    title: 'Django Unchained',
+    director: 'Quentin Tarantino'
+  },
+  {
+    title: 'The Truman Show',
+    director: 'Peter Weir'
+  },
+  {
+    title: 'Mamma Mia!',
+    director: 'Phyllida Lloyd'
+  },
+  {
+    title: 'The Green Mile',
+    director: 'Frank Dabrabont'
+  },
+  {
+    title: 'The Hunger Games',
+    director: 'Francis Lawrence'
+  },
+  {
+    title: 'The Conjuring',
+    director: 'James wan'
+  }
+];
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Welcome to my book club!\n');
-}).listen(8080);
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my top 10 movies!');
+});
 
-console.log('My first Node test server is running on Port 8080.');
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
