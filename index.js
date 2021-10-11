1a069bbb4543d771a81866d3daa04de3be0b1456
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-11 08:56:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express'),
   morgan = require('morgan');
+const { rest } = require('lodash');
 
 const app = express();
-
-app.use(morgan('common'));
 
 let topMovies = [
   {
@@ -48,13 +47,68 @@ let topMovies = [
   }
 ];
 
-// GET requests
+app.use(morgan('common'));
+
+// GET welcome message
 app.get('/', (req, res) => {
-  res.send('Welcome to my top 10 movies!');
+  res.send('Welcome to my movie database!');
 });
 
+// GET list of all movies
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
+});
+
+//GET data about a single movie by title
+app.get('movies/:title', (req, res) => {
+  res.json(movies.find((movie) =>
+  { return movie.title === req.params.title}));
+});
+
+//GET data about a genre by name
+app.get('genres/:genreName', (req, res) => {
+  res.json(movies.find((movie) =>
+  { return movie.genre === req.params.genre }));
+});
+
+//GET data about a director by name
+app.get('directors/:directorName', (req, res) => {
+  res.json(movies.find((movie) =>
+  { return movie.director === req.params.director }));
+});
+
+//POST - add new user
+
+app.post('/register', (req, res) => {
+let newUser = req.body;
+
+if (!newUser.name) {
+  const message = 'Missing name in request body';
+  rest.status(400).send(message);
+} else {
+  newUser.id = uuid.v4();
+  users.push(newUser);
+  res.status(201).send(newUser);
+}
+});
+
+//PUT - update user name
+
+app.put('/users/:name', (req, res) => {
+  let user = user.find((user) => { return user.name === req.params.name});
+  res.send('user name has been updated')
+});
+
+//DELETE movie from favorite list of user
+
+app.delete('/users/:name/favoriteList', (req, res) => {
+  res.send('movie from favorite list has been succesfully deleted')
+});
+
+//DELETE user
+
+app.delete('/users/:name', (req, res) => {
+  res.send('user has been succesfully deleted')
 });
 
 // USE requests
