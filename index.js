9cbe2ba475cb2ade388c5d58cb896ae593498484
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-25 11:43:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,29 @@ app.get('/directors/:name', (req, res) => {
 
 //POST - add new user
 app.post('/users', (req, res) => {
-  res.send('New user has successfully been added');
+  Users.findOne({ Username: req.body.Username})
+  .then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.Username + 'already exists');
+    } else {
+      Users
+      .create({
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then((user) => {res.status(201).json(user) })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      })
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
 });
 
 //PUT - update user name
