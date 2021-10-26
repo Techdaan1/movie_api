488a91502746ba92d5e105ed5966cb905346870d
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-26 13:53:17 +0200

    Update models.js

--- models.js
+++ models.js
@@ -27,8 +27,28 @@ let userSchema = mongoose.Schema({
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
+//creating genreSchema
+let genreSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Description: {type: String, required: true}
+});
+
+//creating directorSchema
+let directorSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Bio: {type: String, required: true},
+    Birthdate: {type: Date, required: true},
+    Death: String
+});
+
+
+
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = User;
+module.exports.Director = User;
