517266c1be6beb0499c87efa0bcec5409c9db887
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-26 12:08:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,17 @@ app.get('/users', (req, res) => {
     });
 });
 
-
+//GET a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 //POST - add new user
 app.post('/users', (req, res) => {
