ddb3a1d8086347a01636cd586e93c5ca612da749
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-26 10:31:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 
 // GET list of all movies
 app.get('/movies', (req, res) => {
-  movies.find()
+  Movies.find()
   .then(movies) => {
     res.status(201).json(movies);
   })
