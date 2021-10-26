45de7edaa76d811974224a14c482f5a8713414d2
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-26 10:43:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ app.get('/', (req, res) => {
 });
 
 // GET list of all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', function (req, res) {
   Movies.find()
-  .then((movies) => {
+  .then(function (movies) {
     res.status(201).json(movies);
   })
-  .catch((err) => {
+  .catch(function(err) {
     console.error(err);
     res.status(500).send('Error: ' + err);
   });
