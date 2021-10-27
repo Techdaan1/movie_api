1b8c4a3d3f8f8ef9e3219e4c38d9082f6a0c066b
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-27 09:11:05 +0200

    Update models.js

--- models.js
+++ models.js
@@ -27,32 +27,9 @@ let userSchema = mongoose.Schema({
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
-/** For your current project you dont need this:
-//creating genreSchema
-let genreSchema = mongoose.Schema({
-    Name: {type: String, required: true},
-    Description: {type: String, required: true}
-});
-
-//creating directorSchema
-let directorSchema = mongoose.Schema({
-    Name: {type: String, required: true},
-    Bio: {type: String, required: true},
-    Birthdate: {type: Date, required: true},
-    Death: String
-});
- **/
-
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
-/** same for this
-let Genre = mongoose.model('Genre', genreSchema);
-let Director = mongoose.model('Director', directorSchema);
-**/
+
 //exports models
 module.exports.Movie = Movie;
 module.exports.User = User;
-/**
-module.exports.Genre = Genre;
-module.exports.Director = Director;
-**/
