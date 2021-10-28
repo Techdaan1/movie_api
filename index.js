6561b093cf34e3069f68d1e8c253a71ba51ec249
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-28 12:02:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 
 //allowing access only from origins specified
-let allowedOrigins = 'http://localhost:8080';
+let allowedOrigins = ['http://localhost:8080'];
 
 app.use(cors({ 
   origin: (origin, callback) => {
