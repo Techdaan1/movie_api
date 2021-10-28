d4c25b70f5f83c7e4432b121e0b74eb4d1f19c9f
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-10-28 11:55:08 +0200

    added cors

--- index.js
+++ index.js
@@ -26,7 +26,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //activating cors (cross origin recourse sharing)
 const cors = require('cors');
-app.use(cors());
+
+//allowing access only from origins specified
+let allowedOrigins = ['http://localhost:8080'];
+
+app.use(cors({ 
+  origin: (origin, callback) => {
+    if(origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //calling authorization and passport
 let auth = require('./auth')(app);
